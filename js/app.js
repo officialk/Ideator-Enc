1211c4281b490897e8e963ce8c0b05d246ec4820
karthik koppaka <officialkarthikk@gmail.com>
2020-03-02 22:38:37 +0530

    added functions to add,remove and view ideas(also projects)

--- js/app.js
+++ js/app.js
@@ -9,13 +9,7 @@ const firebaseConfig = {
     appId: "1:337836532873:web:38436b11705df954df5258"
 };
 var db;
-const encryptionFunctions = [
-
-];
-const decryptionFunctions = [
-
-];
-
+var curProject, curProjectType;
 $(document).ready(function () {
     firebase.initializeApp(firebaseConfig);
     db = firebase.firestore();
@@ -24,6 +18,7 @@ $(document).ready(function () {
     }
 });
 
+/*UI AND OTHER STUFF RELATED FUNCTIONS*/
 const login = () => {
     var provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope('profile');
@@ -60,7 +55,7 @@ const loadDisplay = () => {
     document.getElementsByTagName("main")[0].classList.remove("hide");
     document.getElementsByTagName("footer")[0].classList.remove("hide");
     document.getElementsByTagName("extras")[0].innerHTML = "";
-    loadIdeas();
+    loadProjectList();
     loadMessages();
 }
 
@@ -72,79 +67,29 @@ const initMaterial = () => {
     $('select').formSelect();
 }
 
-const loadIdeas = () => {
-    document.getElementById("public").innerHTML= "";
-    document.getElementById("private").innerHTML= "";
-    db
-        .collection("ideas")
-        .get()
-        .then(ideaList => {
-            ideaList.forEach(doc => {
-                let idea = doc.data();
-                if (idea.type == "private" && idea.creatorId == data.uid) {
-                    let html = `<div class="card idea col s12 m6 l4">
-                                    <div class="card-title">${sjcl.decrypt(data.pin+data.name+data.uid+idea.date,idea.title)}</div>
-                                    <div class="card-content">
-                                        ${sjcl.decrypt(data.pin+data.name+data.uid+idea.date,idea.description)}
-                                        <!--Idea Count::${idea.count}-->
-                                        <br>
-                                        ${idea.date}
-                                    </div>
-                                    <div class="card-action">
-                                        <a class="btn-floating tooltipped" data-position="top" data-tooltip="Add Idea"><i class="material-icons">lock</i></a>
-                                        <a class="btn-floating tooltipped" data-position="top" data-tooltip="Delete Idea" onclick="deleteIdea('${doc.id}')"><i class="material-icons">delete</i></a>
-                                        <a class="btn-floating disabled tooltipped" data-position="bottom" data-tooltip="Begin Work"><i class="material-icons">airplay</i></a>
-                                    </div>
-                                </div>`;
-                    document.getElementById("private").insertAdjacentHTML("beforeend", html);
-                } else {
-                    let html = `<div class="card idea col s12 m6 l4">
-                                    <div class="card-title">${sjcl.decrypt(idea.creatorId+idea.creatorName+idea.date,idea.title)}</div>
-                                    <div class="card-content">
-                                        ${sjcl.decrypt(idea.creatorId+idea.creatorName+idea.date,idea.description)}
-                                        <br>
-                                        ${idea.creatorName}::${idea.date}
-                                        <br>
-                                        <!--Idea Count::${idea.count}-->
-                                    </div>
-                                    <div class="card-action">
-                                        <a class="btn-floating tooltipped" data-position="top" data-tooltip="Add Idea"><i class="material-icons">add</i></a>`;
-                    if (idea.creatorId == data.uid) {
-                        html += `<a class="btn-floating tooltipped" data-position="top" data-tooltip="Delete Idea" onclick="deleteIdea('${doc.id}')"><i class="material-icons">delete</i></a>`;
-                    }
-                    html += `<a class="btn-floating disabled tooltipped" data-position="top" data-tooltip="Begin Work"><i class="material-icons">airplay</i></a></div></div>`;
-                    document.getElementById("public").insertAdjacentHTML("beforeend", html);
-                }
-            });
+const getDate = () => {
+    var today = new Date();
+    var date =
+        today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+    var time =
+        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    return date + " " + time;
+};
+
+const logout = () => {
+    firebase
+        .auth()
+        .signOut()
+        .then(function () {
+            localStorage.setItem("data", "{}");
+            location.reload();
         })
-    initMaterial();
-}
+        .catch(function (error) {
+            // An error happened.
+        });
+};
 
-const deleteIdea = (uid) => {
-    console.log(uid);
-}
-
-const addIdea = () => {
-    let desc = document.getElementById("ideaDescription").value;
-    let title = document.getElementById("ideaTitle").value;
-    let type = document.getElementById("ideaType").value;
-    let date = getDate();
-    let key = data.uid+data.name+date;
-    if (type == "private") {
-        key = data.pin+data.name+data.uid+date;
-    }
-    db.collection("ideas").add({
-        creatorId: data.uid,
-        creatorName: data.name,
-        date: date,
-        description: sjcl.encrypt(key,desc),
-        title: sjcl.encrypt(key,title),
-        type: type
-    })
-    $("#addIdea").modal("close");
-    loadIdeas();
-}
-
+/*MESSAGES RELATED FUNCTIONS*/
 const loadMessages = () => {
     document.getElementById("messageDisplay").innerHTML = "";
     db.collection("messages")
@@ -179,28 +124,157 @@ const sendMessage = () => {
     db.collection("messages").add({
         sender: data.name,
         date: date,
-        content: sjcl.encrypt(date+data.name,mssg)
+        content: sjcl.encrypt(date + data.name, mssg)
     })
 }
 
-const getDate = () => {
-    var today = new Date();
-    var date =
-        today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-    var time =
-        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    return date + " " + time;
-};
-
-const logout = () => {
-    firebase
-        .auth()
-        .signOut()
-        .then(function () {
-            localStorage.setItem("data", "{}");
-            location.reload();
+/*PROJECT RELATED FUNCTIONS*/
+const loadProjectList = () => {
+    document.getElementById("public").innerHTML = "";
+    document.getElementById("private").innerHTML = "";
+    db
+        .collection("projects")
+        .get()
+        .then(ideaList => {
+            ideaList.forEach(doc => {
+                let idea = doc.data();
+                if (idea.type == "private" && idea.creatorId == data.uid) {
+                    let html = `<div class="card idea col s12 m6 l4">
+                                    <div class="card-title">${sjcl.decrypt(data.pin+data.name+data.uid+idea.date,idea.title)}</div>
+                                    <div class="card-content">
+                                        ${sjcl.decrypt(data.pin+data.name+data.uid+idea.date,idea.description)}
+                                        <!--Idea Count::${idea.count}-->
+                                        <br>
+                                        ${idea.date}
+                                    </div>
+                                    <div class="card-action">
+                                        <a class="btn-floating tooltipped" data-position="top" data-tooltip="Add Idea" onclick="curProject='${doc.id}';curProjectType='private';$('#addIdeaModal').modal('open');"><i class="material-icons">add</i></a>
+                                        <a class="btn-floating tooltipped" data-position="top" data-tooltip="Delete Project" onclick="deleteProject('${doc.id}')"><i class="material-icons">delete</i></a>
+                                        <a class="btn-floating tooltipped" data-position="bottom" data-tooltip="View Idea List" onclick="curProject='${doc.id}';viewIdeaList('${doc.id}')"><i class="material-icons">remove_red_eye</i></a>
+                                    </div>
+                                </div>`;
+                    document.getElementById("private").insertAdjacentHTML("beforeend", html);
+                } else {
+                    let html = `<div class="card idea col s12 m6 l4">
+                                    <div class="card-title">${sjcl.decrypt(idea.creatorId+idea.creatorName+idea.date,idea.title)}</div>
+                                    <div class="card-content">
+                                        ${sjcl.decrypt(idea.creatorId+idea.creatorName+idea.date,idea.description)}
+                                        <br>
+                                        ${idea.creatorName}::${idea.date}
+                                        <br>
+                                        <!--Idea Count::${idea.count}-->
+                                    </div>
+                                    <div class="card-action">
+                                        <a class="btn-floating tooltipped" data-position="top" data-tooltip="Add Idea" onclick="curProject='${doc.id}';curProjectType='public';$('#addIdeaModal').modal('open');"><i class="material-icons">add</i></a>`;
+                    if (idea.creatorId == data.uid) {
+                        html += `<a class="btn-floating tooltipped" data-position="top" data-tooltip="Delete Idea" onclick="deleteProject('${doc.id}')"><i class="material-icons">delete</i></a>`;
+                    }
+                    html += `<a class="btn-floating tooltipped" data-position="bottom" data-tooltip="View Idea List" onclick="curProject='${doc.id}';viewIdeaList('${doc.id}')"><i class="material-icons">remove_red_eye</i></a>`;
+                    document.getElementById("public").insertAdjacentHTML("beforeend", html);
+                }
+            });
+            initMaterial();
         })
-        .catch(function (error) {
-            // An error happened.
-        });
-};
+}
+
+const deleteProject = id => {
+    if (confirm("DO YOU REALY WANT TO DELETE THE PROJECT AS IT IS A IRREVERSIBLE PROCESS")) {
+        db
+            .collection("projects")
+            .doc(id)
+            .delete()
+            .then(e => {
+                alert("Project Deleted");
+                loadProjectList();
+            })
+    }
+}
+
+const addProject = () => {
+    let desc = document.getElementById("projectDescription").value;
+    let title = document.getElementById("projectTitle").value;
+    let type = document.getElementById("projectType").value;
+    let date = getDate();
+    let key = data.uid + data.name + date;
+    if (type == "private") {
+        key = data.pin + data.name + data.uid + date;
+    }
+    db.collection("projects").add({
+        creatorId: data.uid,
+        creatorName: data.name,
+        date: date,
+        description: sjcl.encrypt(key, desc),
+        title: sjcl.encrypt(key, title),
+        type: type
+    })
+    $("#addProject").modal("close");
+    loadProjectList();
+}
+
+/*IDEA RELATED FUNCTIONS*/
+const addIdea = () => {
+    let id = curProject;
+    let title = document.getElementById("ideaTitle").value;
+    let description = document.getElementById("ideaDescription").value;
+    let date = getDate();
+    let key = data.uid + data.name + date;
+    if (curProjectType == "private") {
+        key = data.uid + data.name + data.pin + date;
+    }
+    db.collection(`projects/${curProject}/ideas`).add({
+        title: sjcl.encrypt(key, title),
+        description: sjcl.encrypt(key, description),
+        creatorId: data.uid,
+        creatorName: data.name,
+        date: date,
+        type: curProjectType
+    });
+    $("#addIdeaModal").modal("close");
+}
+
+const deleteIdea = id => {
+    console.log(id);
+    if (confirm("DO YOU REALY WANT TO DELETE THE IDEA AS IT IS A IRREVERSIBLE PROCESS")) {
+        db
+            .collection("projects")
+            .doc(`${curProject}`)
+            .collection("ideas")
+            .doc(id)
+            .delete()
+            .then(e => {
+                alert("Idea Deleted");
+                viewIdeaList();
+            })
+    }
+}
+
+const viewIdeaList = id => {
+    console.log(id);
+    $("#ideaListModal").modal("open");
+    db.collection(`projects/${id}/ideas`)
+        .get()
+        .then(ideas => {
+            //            console.log(ideas.docs());
+            let html = ``;
+            ideas.forEach(ideaDoc => {
+                console.log(ideaDoc.id);
+                idea = ideaDoc.data();
+                let key = idea.creatorId + idea.creatorName + idea.date;
+                if (idea.type == "private") {
+                    key = data.uid + data.name + data.pin + idea.date;
+                }
+                html += `<li>
+                            <div class="collapsible-header"><i class="material-icons">lightbulb_outline</i>${sjcl.decrypt(key,idea.title)}</div>
+                            <div class="collapsible-body">
+                                <span>${sjcl.decrypt(key,idea.description)}</span>
+                                <p><span>Suggestor::${(idea.creatorName==data.name)?"You":idea.creatorName}</span><br><span>date::${idea.date}</span></p>`;
+                if (data.uid == idea.creatorId) {
+                    html += `<p><div class="btn-floating" onclick="deleteIdea('${ideaDoc.id}')"><i class="material-icons">delete</i></div></p>`
+                }
+                html += `</div></li>`;
+            })
+            document.getElementById("ideaListDisplay").innerHTML = html;
+            $('.collapsible').collapsible();
+        })
+
+}
