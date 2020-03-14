b1d130c3bcb193ebd37963d00f9d48336c5aee1a
officialk <officialkarthikk@gmail.com>
2020-03-14 17:52:35 +0530

    setting the demo test base

--- js/firebase.js
+++ js/firebase.js
@@ -1,10 +1,9 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCunSys_ABIxjF7KiLsEeBZ35XwNMjB7gQ",
-    authDomain: "ideator-live.firebaseapp.com",
-    databaseURL: "https://ideator-live.firebaseio.com",
-    projectId: "ideator-live",
-    storageBucket: "ideator-live.appspot.com",
-    messagingSenderId: "520430350326",
-    appId: "1:520430350326:web:76dede2e74e14f565e21d7",
-    measurementId: "G-HVQJ271469"
+    apiKey: "AIzaSyDLKOTc-PKbspDjOFlpkiMDYHuN_77DJpo",
+    authDomain: "ideator-c9f1d.firebaseapp.com",
+    databaseURL: "https://ideator-c9f1d.firebaseio.com",
+    projectId: "ideator-c9f1d",
+    storageBucket: "ideator-c9f1d.appspot.com",
+    messagingSenderId: "337836532873",
+    appId: "1:337836532873:web:38436b11705df954df5258"
 };
