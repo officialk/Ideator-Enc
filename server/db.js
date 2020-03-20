a6a6d73eecf8e028b8493149d4fa578122c835c9
officialk <officialkarthikk@gmail.com>
2020-03-20 18:26:16 +0530

    converted to pool connnection

--- server/db.js
+++ server/db.js
@@ -3,49 +3,30 @@ const randomstring = require("randomstring");
 const dbKey = require('./keys')[0];
 const tables = require('./keys')[1];
 
-var con;
+var con = mysql.createPool(dbKey);
 
-function handleDisconnect() {
-    con = mysql.createConnection(dbKey);
-    con.connect(function (err) {
-        if (err) {
-            setTimeout(handleDisconnect, 5000);
-        } else {
-            con.primKey = () => {
-                return randomstring.generate();
-            };
+con.primKey = () => {
+    return randomstring.generate();
+};
 
-            con.insert = (table, values, fun) => {
-                let query = `insert into ${tables[table]} set ?`;
-                con.query(query, values, fun);
-            }
-
-            con.read = (table, columns, where, fun) => {
-                let query = `select ${columns} from ${tables[table]} where ${where}`;
-                con.query(query, fun);
-            }
-
-            con.update = (table, set, where, fun) => {
-                let query = `update ${tables[table]} set ? where ${where}`;
-                con.query(query, set, fun);
-            }
-
-            con.del = (table, where, fun) => {
-                let query = `delete from ${tables[table]} where ${where}`;
-                con.query(query, fun);
-            }
-        }
-    });
-    con.on('error', function (err) {
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect();
-        } else {
-
-        }
-    });
-
+con.insert = (table, values, fun) => {
+    let query = `insert into ${tables[table]} set ?`;
+    con.query(query, values, fun);
 }
 
-handleDisconnect();
+con.read = (table, columns, where, fun) => {
+    let query = `select ${columns} from ${tables[table]} where ${where}`;
+    con.query(query, fun);
+}
+
+con.update = (table, set, where, fun) => {
+    let query = `update ${tables[table]} set ? where ${where}`;
+    con.query(query, set, fun);
+}
+
+con.del = (table, where, fun) => {
+    let query = `delete from ${tables[table]} where ${where}`;
+    con.query(query, fun);
+}
 
 module.exports = con;
